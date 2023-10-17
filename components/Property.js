40391d1b89fe7f86c442bd59da14236e5c70a511
Gambhir Dhruv <gambhirdhruv03@gmail.com>
2023-10-17 19:22:38 +0800

    Working without mobile

--- components/Property.js
+++ components/Property.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import Image from 'next/image';
 const Property = ({ property }) => {
+  console.log("Property")
+  console.log(property.imageurl)
   return (
     <div className="property border p-4 m-4">
-      <img src={property.imageurl} alt={property.title} className="max-w-full" />
-      <h2 className="text-xl font-semibold">{property.title}</h2>
+      <img src={property.imageurl} alt='image' className='h-30 w-30' /> 
+      {/* <Image src={property.imageurl} alt={property.title} width={20} height={20}/> */}
+      <h2 className="text-xl font-semibold text-black">{property.title}</h2>
       <p className="text-gray-600">{property.description}</p>
       <p className="text-purple-700">Price: ${property.price}</p>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,21 @@ import Property from '../components/Property';
 
 const Home = () => {
   const [properties, setProperties] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
     async function fetchProperties() {
       try {
         const response = await fetch(
-          'https://m9ojazlunf.execute-api.ap-southeast-1.amazonaws.com/test'
+          `https://m9ojazlunf.execute-api.ap-southeast-1.amazonaws.com/test?offset=${offset}&limit=${limit}`
         );
 
         if (response.ok) {
           const data = await response.json();
-          setProperties(data.list);
+          setProperties((prevProperties) => [...prevProperties, ...data.list]);
+          setHasMore(data.hasMore);
         } else {
           console.error('Failed to fetch data');
         }
@@ -23,7 +27,27 @@ const Home = () => {
     }
 
     fetchProperties();
-  }, []);
+  }, [offset, limit]);
+
+  function handleLoadMore() {
+    if (hasMore) {
+      setOffset((prevOffset) => prevOffset + 4);
+    }
+  }
+
+  function renderLoadMoreButton() {
+    if (hasMore) {
+      return (
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+          onClick={handleLoadMore}
+        >
+          Load More
+        </button>
+      );
+    }
+    return null;
+  }
 
   return (
     <div className="container mx-auto p-4">
@@ -33,6 +57,14 @@ const Home = () => {
           <Property key={property.id} property={property} />
         ))}
       </div>
+      {hasMore && (
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+          onClick={handleLoadMore}
+        >
+          Load More
+        </button>
+      )}
     </div>
   );
 };
