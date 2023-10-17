b69479419e593680c27a6664584b70191a4f5017
Gambhir Dhruv <gambhirdhruv03@gmail.com>
2023-10-17 20:19:45 +0800

    Final 2.0

--- components/Property.js
+++ components/Property.js
@@ -4,12 +4,12 @@ const Property = ({ property }) => {
   console.log("Property")
   console.log(property.imageurl)
   return (
-    <div className="property border p-4 m-4">
-      <img src={property.imageurl} alt='image' className='h-30 w-30' /> 
+    <div className="property p-4 m-4">
+      <img src={property.imageurl} alt='image' style={{width: 336, height: 200, borderRadius: 10}} /> 
       {/* <Image src={property.imageurl} alt={property.title} width={20} height={20}/> */}
       <h2 className="text-xl font-semibold text-black">{property.title}</h2>
       <p className="text-gray-600">{property.description}</p>
-      <p className="text-purple-700">Price: ${property.price}</p>
+      <p className="text-gray-700">Price: ${property.price}</p>
     </div>
   );
 };
